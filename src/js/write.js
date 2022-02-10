e46f69f1a020e3bfa70b92a2427d78441e8576ca
Ksmith106 <77019810+Ksmith106@users.noreply.github.com>
2022-02-09 22:39:17 -0700

    coffee shop styles

--- src/js/write.js
+++ src/js/write.js
@@ -13,34 +13,20 @@ document.forms["coffeeForm"].addEventListener("submit", onAddCoffee);
   
 
    function onImageSelected(e) {
-    //selected file
-    // file objets   [fileObj, fileObj, fileObj]
     let file = e.target.files[0];
     console.log(file)
-    // update the display with the requested image
     document.querySelector(".display img").src = URL.createObjectURL(file);
-     
     }
-
     async function uploadNewcoffeeSelection() {
-        // form data
         const coffee = document.querySelector('#coffeeName').value.trim();
-        const file = document.querySelector('#rentalImage').files[0]
-        
-        // paths to the data to write
-        const imageRef =     storageRef( storage, `images/${file.name}`);
-        const dataRef =  databaseRef( db, 'rentals')
-
-        // uploading file to the storage bucket
+        const file = document.querySelector('#coffeeImage').files[0]
+        const imageRef = storageRef( storage, `images/${file.name}`);
+        const dataRef =  databaseRef( db, 'coffee')
         const uploadResult = await uploadBytes(imageRef, file);
-        // url to the image stored in storage bucket
         const urlPath =  await getDownloadURL(imageRef) 
-        // path on the storage bucket to the image
         const storagePath = uploadResult.metadata.fullPath;
-
-        // firebase unique key
         const itemRef = await push(dataRef)
-        
+
         set(itemRef,{
            key:itemRef.key,
            sku:`jhvr${itemRef.key}`,
